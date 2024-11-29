e19c2bf29212a1d8712305f4f03f76e187295d87
varun789-mx <varunsh153@gmail.com>
2024-11-30 01:47:00 +0530

    fixed the issue with old/new password

--- backend/routes/update.js
+++ backend/routes/update.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { User_Schema } from "../db.js";
-import updateschema from "./zodschemas/uservalidate.js";
+// import validation from "./zodschemas/uservalidate.js";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import { z } from "zod";
 const saltRounds = 10;
 
 const update = express();
@@ -10,26 +11,49 @@ const update = express();
 update.use(express.json());
 update.use(cors());
 
+const validation = z.object({
+  Email: z.string().email().min(5),
+  Password: z.string().min(5),
+  New_Password: z.string().min(5),
+});
 update.put("/update", async (req, res) => {
   try {
     const payload = {
-      _id: req.body._id,
+      Email: req.body.Email,
       Password: req.body.Password,
+      New_Password: req.body.New_Password,
     };
-    const valid_data = updateschema.safeParse(payload);
+    const valid_data = validation.safeParse(payload);
     if (!valid_data.success) {
       return res.status(411).json({
         msg: "Incorrect inputs",
-        issues: valid_data.error.issues,
+        issue: valid_data.error.issues,
       });
     }
+    const user = await User_Schema.findOne({
+      Email: valid_data.data.Email,
+    });
+    const valid_Pass = await bcrypt.compare(
+      valid_data.data.Password,
+      user.Password
+    );
+    if (!valid_Pass) {
+      return res.status(404).json({
+        msg: "Incorrect Email or password",
+      });
+    }
+
+    const hash = await bcrypt.hash(valid_data.data.New_Password, saltRounds);
+    user.Password = hash;
+    await user.save();
     return res.status(200).json({
       msg: "Success",
+      new_password: hash,
     });
   } catch (error) {
+    console.log("error occurred", error);
     return res.status(411).json({
       msg: "Something went wrong",
-      error: error,
     });
   }
 });
