5849c2076be15619bce5a47ffba0a9140013e5c5
varun789-mx <varunsh153@gmail.com>
2024-11-29 23:45:58 +0530

    test 1

--- backend/routes/update.js
+++ backend/routes/update.js
@@ -15,49 +15,25 @@ update.put("/update", async (req, res) => {
     const payload = {
       _id: req.body._id,
       Password: req.body.Password,
-      confirm_Password: req.body.confirm_Password,
     };
-    console.log(payload);
-    const parsed_payload = updateschema.safeParse(payload);
-    if (!parsed_payload.success) {
+    const valid_data = updateschema.safeParse(payload);
+    if (!valid_data.success) {
       return res.status(411).json({
         msg: "Incorrect inputs",
-        issues: parsed_payload.error.issues,
+        issues: valid_data.error.issues,
       });
     }
-    if (
-      !parsed_payload.data.Password === parsed_payload.data.confirm_Password
-    ) {
-      return res.status(411).json({
-        msg: "Password and confirm password doesn't match",
-      });
-    }
-    const hashed = await bcrypt.hash(parsed_payload.data.Password, saltRounds);
-    const user = User_Schema.findOne({
-      _id: parsed_payload.data._id,
-    });
-    if (!user) {
-      return res.json(411).json({
-        msg: "user not found",
-      });
-    }
-    user.Password = hashed;
-    await User_Schema.updateOne(
-      { _id: user._id },
-      { Password: user.Password }
-    );
-
-    return res.status(202).json({
-      msg: "Password changed succesfully"
+    return res.status(200).json({
+      msg: "Success",
     });
   } catch (error) {
-    console.log(error);
     return res.status(411).json({
-      msg: "Request unsuccesfull",
+      msg: "Something went wrong",
+      error: error,
     });
   }
 });
 
 update.listen(9000, () => {
-  console.log(`started on 9000`);
+  console.log("server started on http://localhost:9000/update");
 });
